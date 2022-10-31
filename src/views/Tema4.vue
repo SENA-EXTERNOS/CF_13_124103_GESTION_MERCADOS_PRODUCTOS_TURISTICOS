bee8a2680a40fa49cb4b802ff482a46d6d3f63b2
deividramirezarias <deivid.innovatec@gmail.com>
2022-10-31 11:59:01 -0500

    se vincula la actividad didáctica
    se vincula la actividad didáctica

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,7 +5,7 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Titulo de primer nivel
+      h1 Patrones de conducta
 
     .row.justify-content-center.my-5
       .col-lg-10
