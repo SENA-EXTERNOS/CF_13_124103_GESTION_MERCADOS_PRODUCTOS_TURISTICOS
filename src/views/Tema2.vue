b5a77cf087c381286db2fdbce1dbab4648c01fb7
deividramirezarias <deivid.innovatec@gmail.com>
2023-03-29 22:21:34 -0500

    ajustes
    ajustes

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -128,7 +128,7 @@
     .row.my-5
       figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/sxtcmic8eHA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     h3 Bases comunes entre las diferentes comunidades virtuales  
     .row.justify-content-center.my-5
